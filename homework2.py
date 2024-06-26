c5c7b3558a4b01be1e1716b8f9df107b723dac1c
sadhguru69 <golf3_95@bk.ru>
2024-06-26 12:20:00 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+amount_time = 1.5
+vsego_dz = 12
+name_course = "Python"
+time_work = (amount_time/vsego_dz)
+print(time_work)
+print("Курс:",name_course,",""всего задач:",vsego_dz,",""затрачено часов",amount_time,",""среднее время выполнения",time_work,"часа" )
